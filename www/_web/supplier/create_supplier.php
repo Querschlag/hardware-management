eed4038c6b4cf7bbd71ae1aca40fa4bb28d4bb6d
PhilippSchmidkunz <philippschmidkunz@googlemail.com>
2013-07-25 09:56:05 +0200

    Added supplier user interface (clickdummie without functionaliti).

---
+++ www/_web/supplier/create_supplier.php
@@ -0,0 +1,25 @@
+<!-- Refactor this to be created dynamically -->
+<div id="breadcrumb_nav">
+	<ul>
+		<li><a href="index.php">Startseite</a></li>
+		<li>>> <a href="index.php?mod=user">Lieferant</a></li>
+		<li>>> <a href="index.php?mod=createUser">Lieferant anlegen</a></li>
+	</ul>
+</div>
+<div id="module">
+	<h2>Lieferant anlegen</h2>
+	<form action="index.php?mod=supplier" method="post">
+		<p>Firmenname</p><input name="name" type="text"/>
+		<p>Postleitzahl</p><input name="Plz" type="text"/>
+		<p>Ort</p><input name="Ort" type="text"/>
+		<p>Telefon</p><input name="Tel" type="text"/>
+		<p>Mobiltelefon</p><input name="Mobil" type="text"/>
+		<p>Fax</p><input name="fax" type="text"/>
+		<p>Email</p><input name="email" type="email"/>
+		
+		<br>
+		<br>
+		<input name="btnSubmit" type="submit" value="Anlegen" />
+		<input  type="button" value="Abbrechen" onClick="location.href = 'index.php?mod=supplier'" />
+	</form>
+</div>
