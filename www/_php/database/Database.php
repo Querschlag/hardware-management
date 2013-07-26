a75d29d5aaa3492c8f551f6a6449f843ad8ad9a4
Adrian Geuss <adriangeuss@gmail.com>
2013-07-26 14:19:22 +0200

    Suppressed error in Database.php when connection to main server fails.

--- www/_php/database/Database.php
+++ www/_php/database/Database.php
@@ -17,8 +17,8 @@
 	{
 		public function __construct() 
 		{
-			if(!(mysql_connect("10.9.4.51", "itv_v1", "")))
-			{				
+			if(!(@mysql_connect("10.9.4.51", "itv_v1", "")))
+			{			
 				mysql_connect("localhost", "itv_v1", "");
 			}
 			mysql_select_db("itv_v1");
