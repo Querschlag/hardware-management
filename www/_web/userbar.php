09dec7de0874da3c39071a9a789627143b5fef67
Adrian Geuss <adriangeuss@gmail.com>
2013-07-27 21:09:17 +0200

    Fixed a chrome glitch in userbar.

--- www/_web/userbar.php
+++ www/_web/userbar.php
@@ -1,8 +1,5 @@
 <ul>
-	<li class="user">
-		<?php echo $_SESSION['username']; ?>
-		<span class="arrowDown">&#x25bc;</span>
-	</li>
+	<li class="user"><?php echo $_SESSION['username']; ?><span class="arrowDown">&#x25bc;</span></li>
 	<hr>
 	<li class="logout"><a href="index.php?logout">Ausloggen</a></li>
 </ul>
